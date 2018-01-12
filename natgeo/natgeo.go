2b92ddf2a9a3756c1b67f0c941425f0e6352fe2a
Max Podkolzin <mpodkolzin87@gmail.com>
2018-01-11 21:05:29 -0800

    (edit) natgeo update

--- natgeo/natgeo.go
+++ natgeo/natgeo.go
@@ -1,5 +1,28 @@
 package natgeo
 
 import (
-	"fmt"
+	"errors"
+	"os"
 )
+
+type ConfigDiscoveryServer interface {
+	GetValue(name string) (string, error)
+}
+
+var ConfigServer ConfigDiscoveryServer
+
+type EnvVarConfigServer struct{}
+
+func (srv *EnvVarConfigServer) GetValue(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Name cannot be empty")
+	}
+
+	res := os.Getenv(name)
+	return res, nil
+
+}
+
+func init() {
+	ConfigServer = &EnvVarConfigServer{}
+}
